0a46571a7c2a985eccba0b04dee014e2f4e4b717
Jess Rezac <jess.rezac@gmail.com>
2019-05-28 21:01:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    anagrams = []
+
+    possible_anagrams.map do |possible_anagram|
+      anagrams << possible_anagram if possible_anagram.split("").sort == @word.split("").sort
+    end
+
+    anagrams
+  end
+end
