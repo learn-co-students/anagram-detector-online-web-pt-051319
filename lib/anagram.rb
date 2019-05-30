964ea56ed8d3f51d65c9d6697059c23ccc1aeed2
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-29 22:51:43 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(contenders)
+		contenders.select do |contender|
+			contender.split("").sort == @word.split("").sort
+		end
+	end
+end
