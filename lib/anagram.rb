102c9bcf1225779665000d1b1fade782ed6861b7
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-29 21:56:06 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(tests)
+    found = []
+     tests.each do |item|
+       if item.chars.sort == @word.chars.sort
+         found << item
+        end
+      end
+      return found
+    end        
+  
+end  
