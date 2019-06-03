6d232ec2cc7766cdfca1fa493e0a8c73861c3321
Jasmine Chen <jasmine1226@gmail.com>
2019-06-03 00:59:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    output = []
+    array.each do |item|
+      if item.split("").sort == word.split("").sort
+        output << item
+      end
+    end
+    output
+  end
+end
