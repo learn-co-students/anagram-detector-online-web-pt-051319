ff8cace084a17b69db0062a30ca88046263a660a
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-07 18:21:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+   # array.find {|w| w.split(" ").sort == self.word.split(" ").sort}
+   new_array = []
+   array.each do |ana|
+    if ana.split("").sort == self.word.split("").sort
+      new_array << ana
+    end 
+  end
+  new_array
+end
+  
+end 
