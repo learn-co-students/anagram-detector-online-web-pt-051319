d92813758cdbc93b77759db37c7a9bbc03cdaa11
Annia Forney <anniaforney@gmail.com>
2019-06-19 18:47:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,6 @@
 # Your code goes here!
+#Your class, Anagram should take a word on initialization, and instances should respond to a match method that takes an array of possible anagrams. It should return all matches in an array. If no matches exist, it should return an empty array.
+
 require 'pry'
 
 class Anagram
@@ -8,8 +10,8 @@ class Anagram
       @word = word
   end
 
-  def match
-    binding.pry
-  end
-
+  def match(array)
+    @word = @word.chars.sort.join
+    array.select {|word| word.chars.sort.join == @word}
+  end 
 end
